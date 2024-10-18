612e0d4a33cb394f55137453499b3b10eb7fe427
emanoele <162597868+emanoeleosorio@users.noreply.github.com>
2024-10-18 07:28:58 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,41 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Ordem Paranormal',
+    'O que acontece no final?',
+    'Os personagens Liz, Daniel e Tiago conseguem derrotar o monstro, mas isso resulta na morte de Daniel, e Tiago fica surdo de um ouvido.'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'O Segredo na Floresta',
+    'O que é o santo berço?',
+    'Carregada de mistério e segredos, é uma vila no meio da floresta. A maior parte da série se passa lá, onde o tempo passa mais rápido do que em outros lugares.'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Desconjuração',
+    'O que é cineraria?',
+    'É um elemento relacionado a rituais e práticas ocultas, já foi usado duas vezes, mas a mais importante foi no último episódio.'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Calamidade',
+    'Como a Carina ajuda a equipe na ultima luta?',
+    'Ela usa o livro do Arnaldo e, com isso, consegue remover os rituais de Kian, ajudando a equipe a derrotar o inimigo.'
+)
+
+criaCartao(
+    'O Segredo na Ilha',
+    'Qual a relação entre Milo e Miguel?',
+    ' A relação entre Milo e Miguel é complexa e evolui ao longo da história. Com o tempo, os personagens descobrem que os dois possivelmente são a mesma pessoa.'
+)
+
+criaCartao(
+    'Sinais do Outro Lado',
+    'O que acontece com a Dara no episódio 4?',
+    'Ao final do episódio, o Estrangeiro (entidade) aparece na Fazenda do Adagio para levar a Dara.'
+)
+
+criaCartao(
+    'Quarentena',
+    'Quem revela ser inimigo?',
+    'A Carol, personagem que a Gabi fez, revela ser uma trabalhadora da indústria Panacea.'
 )
